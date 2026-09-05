b21d0cd8745e89761848327b7f9c5e27fbb3c57a
jromero <jromerolagos30@gmail.com>
2026-09-05 10:59:58 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG={
-  API_URL:"PEGA_AQUI_TU_URL_DE_APPS_SCRIPT",
+  API_URL:"https://script.google.com/macros/library/d/1j5jpnanefbiQtRU1GYp9M6vxkI0yELb31u1key7yCXoUhvey1dUX8q36/3",
   APP_NAME:"Gestión de Izaje Mecánico de Cargas",
   MAP_IMAGE_URL:"assets/mapa_planta_nueva.png",
   STANDARD_URL:"assets/DSHIU-S-006.pdf",
